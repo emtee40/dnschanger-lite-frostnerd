da1f2c0f1dc22730435ea9b645fda780d78e711a
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-04-12 19:31:51 +0200

    Added preference opening store page of the full version

--- app/src/main/java/com/frostnerd/dnschangerlight/SettingsActivity.java
+++ app/src/main/java/com/frostnerd/dnschangerlight/SettingsActivity.java
@@ -40,6 +40,18 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 return true;
             }
         });
+        findPreference("full_version").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                String appPackageName = getPackageName().replace("light","");
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+                return true;
+            }
+        });
     }
 
     @Override
